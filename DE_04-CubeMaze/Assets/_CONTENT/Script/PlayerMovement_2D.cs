062b927fba71215392174a2f32de43415db5f5b2
Nihaalknight <fine.history@yahoo.com>
2018-08-25 23:19:55 +0530

    New Changes - 1.3 (Created A moving Script)
    Use "WSAD" To move Arround.

---
+++ DE_04-CubeMaze/Assets/_CONTENT/Script/PlayerMovement_2D.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2D : MonoBehaviour
+{
+
+    Vector2 Playerpos;
+    public int MovementDir;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Playerpos = transform.position;
+        MovementDir = 0;
+    }
+
+    private void Update()
+    {
+        //rb.velocity = Playerpos;
+
+        PlayerController();
+        PlayerMovementController();
+
+    }
+
+    private void PlayerController()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MovementDir = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            MovementDir = -1;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            MovementDir = -2;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            MovementDir = 2;
+        }
+    }
+    private void PlayerMovementController()
+    {
+        if (MovementDir == 1)
+        {
+            Playerpos.y += 0.05f;
+            transform.position = Playerpos;
+        }
+        else if (MovementDir == -1)
+        {
+            Playerpos.y -= 0.05f;
+            transform.position = Playerpos;
+        }
+        else if (MovementDir == -2)
+        {
+            Playerpos.x -= 0.05f;
+            transform.position = Playerpos;
+        }
+        else if (MovementDir == 2)
+        {
+            Playerpos.x += 0.05f;
+            transform.position = Playerpos;
+        }
+    }
+}
